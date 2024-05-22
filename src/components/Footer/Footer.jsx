a93906d03ce462d4a557dce16616a0987085a58e
Theo101 <ted.a.lesser@gmail.com>
2024-05-22 19:15:17 -0400

    third commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__developed_by">Developed by Name Surname</p>
+      <div className="footer__user-container">
+        <p className="footer__year">2022</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import "./Header.css"
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header( {onAddButtonClick, weatherData} ) {
+    const currentDate = new Date().toLocaleString('default', 
+    { month: 'long', 
+        day: 'numeric' });
+
+    return (
+    <header className="header">
+        <img className="header__logo" src={logo} />
+        <p className="header__date-and-location">{currentDate}, {weatherData.city}</p>
+        <button onClick={onAddButtonClick} type="button" className="header__add-clothes-button">+ Add clothes</button>
+        <div className="header__user-container">
+            <p className="header__username">Terence Tegegne</p>
+            <img src={avatar} alt="" className="header__avatar" />
+        </div>
+    </header>
+    );
+}
+
+export default Header;
+
