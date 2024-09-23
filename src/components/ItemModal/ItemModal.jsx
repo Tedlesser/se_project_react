9e55ef52b033c2b999399986527bcc397b50e834
Theodore Lesser <ted.a.lesser@gmail.com>
2024-09-23 13:52:59 -0400

    Update enable delete button

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,9 +1,12 @@
 import "./ItemModal.css";
-import DeleteConfirmModal from "../DeleteConfirmModal/DeleteConfirmModal"
-import React, { useState } from 'react';
-
+import DeleteConfirmModal from "../DeleteConfirmModal/DeleteConfirmModal";
+import React, { useState } from "react";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
+import { useContext } from "react";
 
 function ItemModal({ activeModal, card, onClose, onClick, isAuthenticated }) {
+  const { currentUser } = useContext(CurrentUserContext);
+
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__contents modal__content_type_image">
@@ -15,20 +18,14 @@ function ItemModal({ activeModal, card, onClose, onClick, isAuthenticated }) {
         <img src={card.imageUrl} alt={card.name} className="modal__image" />
         <div className="modal__footer">
           <div className="title__box">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
           </div>
-          {isAuthenticated ? (
-           <button
-           type="button"
-           onClick={onClick}
-           className="delete-card"
-         >
-           Delete Card
-         </button>
-        ) : (
-         null
-        )}
+          {currentUser && currentUser._id === card?.owner ? (
+            <button type="button" onClick={onClick} className="delete-card">
+              Delete Card
+            </button>
+          ) : null}
         </div>
       </div>
     </div>
